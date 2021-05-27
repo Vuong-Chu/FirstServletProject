7e5b85d14b9a17e50ec389e1a3f0781d18c7286b
Vuong-Chu <minhvuong2992@gmail.com>
2021-05-27 08:29:37 +0100

    Commited

--- src/main/java/com/example/FirstServletProject/HelloServlet.java
+++ src/main/java/com/example/FirstServletProject/HelloServlet.java
@@ -15,7 +15,7 @@ public class HelloServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         int num1 = Integer.parseInt(request.getParameter("num1"));
         int num2 = Integer.parseInt(request.getParameter("num2"));
-        int result = num1+num2;
+        int result = num1*num2;
         response.setContentType("text/html");
         // Hello
         PrintWriter out = response.getWriter();
